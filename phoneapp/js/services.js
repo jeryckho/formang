6335d74ceabc8f48380c9e8c7edeb55d227c26d5
jeryckho <jerome.chery@nrco.fr>
2016-02-09 10:42:40 +0100

    + services

---
+++ phoneapp/js/services.js
@@ -0,0 +1,7 @@
+angular.module('phonecatApp')
+.factory('Phone', ['$resource',
+  function($resource){
+    return $resource('phones/:phoneId.json', {}, {
+      query: {method:'GET', params:{phoneId:'phones'}, isArray:true}
+    });
+  }]);
